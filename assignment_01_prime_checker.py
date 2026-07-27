58055780022094f4950d18fd9fa6dba9b8bcaa77
Yajagee-Nakoja-Samuel <nsyajagee002@st.ug.edu.gh>
2026-07-27 15:05:44 +0000

    Update assignment_01_prime_checker.py

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -39,9 +39,9 @@ def is_prime(number):
     if number<2:
         return False
     for i in range(2, int(number**0.5)+1):
-        if number% i==0
+        if number% i==0:
             return False
-     return True
+    return True
 if __name__ == "__main__":
     num=int(input("Enter a number: "))
     if is_prime(num):
